7818bc99a51c7f6313a40cafcd28d1660bad4326
Paras Rawat <parasrawat67@gmail.com>
2021-05-22 10:30:24 +0530

    Adding User Class

---
+++ ShareMarket/Controllers/Api/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using ShareMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShareMarket.Controllers.Api
+{
+    public class UsersController : ApiController
+    {
+        public UsersController()
+        {
+
+        }
+
+        //GET api/customers
+        public IEnumerable<User> GetUsers()
+        {
+            return;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
